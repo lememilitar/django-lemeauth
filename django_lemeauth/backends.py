1fd42002ae204a0e1bcf747d79ca1c509df69da6
George Moura <gwmoura@gmail.com>
2017-04-18 23:56:36 -0300

    remove signals :/

--- django_lemeauth/backends.py
+++ django_lemeauth/backends.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.hashers import check_password
 from django.contrib.auth.backends import ModelBackend
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Permission
 from django.conf import settings
 from lemeauth import LemeAuth
 
@@ -21,12 +21,21 @@ class LemeAuthBackend(ModelBackend):
                 user.is_active = True
                 user.is_superadmin = self.is_superadmin(user)
                 user.save()
+                self.set_default_permissions
             return user
         return None
 
 
     def is_superadmin(self, user):
         return user.username in settings.LEMEAUTH_SUPERADMINS
+
+   def set_default_permissions(self, user):
+       permissions = settings.LEMEAUTH_DEFAULT_PERMISSIONS
+       for permission in permissions:
+           p = Permission.objects.get(codename=permission)
+           user.user_permissions.add(p)
+
+       user.save()
 
 
 class AllowAllUsersLemeAuthBackend(ModelBackend):
